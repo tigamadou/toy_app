01e65e2250968c53d7b64e3c804069b1aca44a58
Amadou Ibrahim <amadou140589@gmail.com>
2019-12-17 21:55:14 +0100

    Validations added for user and micropost models

--- app/models/micropost.rb
+++ app/models/micropost.rb
@@ -1,3 +1,5 @@
 class Micropost < ApplicationRecord
-    validates :content, length: { maximum: 140 }
+    belongs_to :user
+    validates :content, length: { maximum: 140 }, presence: true
+    
 end
